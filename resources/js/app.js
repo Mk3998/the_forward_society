8335ae4ca87d2544d06a9078b2afbf0a60d610b3
Mk <minkhant14616@gmail.com>
2023-03-15 14:24:46 +0630

    first

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+
+// import '././node_modules/font-awesome/css/font-awesome.min.css';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,31 @@
+@extends('templates.master')
+
+@section('title')
+    Dashboard
+@endsection
+
+@section('content')
+    {{-- breadcrumb --}}
+<div class="text-sm breadcrumbs ml-5 pt-5  text-primary">
+    <ul>
+      <li>Dashboard</li>
+    </ul>
+</div>
+<x-card>
+    <x-card-header title="Dashboard">
+        <a  href="{{ route('table') }}" class="btn btn-primary btn-md flex-none mr-3">
+            <i class="fa fa-arrow-circle-o-left"></i>
+            <span>Table</span>
+        </a>
+        <a  href="{{ route('form') }}" class="btn btn-primary btn-md flex-none mr-3">
+            <i class="fa fa-arrow-circle-o-left"></i>
+            <span>Form</span>
+        </a>
+    </x-card-header>
+    <div class="container">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci non voluptas incidunt repellendus nisi commodi error ab architecto similique esse.
+        </p>
+    </div>
+</x-card>
+
+@endsection
